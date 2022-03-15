80b7ab75054945e35edfd6002811ba7615762f43
Mateo Carreras <mateo.carreras@gmail.com>
2022-03-15 15:24:45 -0700

    Added month enum with translation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+//! A Scottish Gaelic (Gàidhlig) date library.
+//!
+//! # Language Use
+//! As this is a bi-lingual library, both languages are used in places on the
+//! library. All variables, structures, fields are named in English, because
+//! this library is meant to be as accessible to as many people as possible.
+//! That being said, as much of the documentation as possible will be in both
+//! languages.
+
+pub mod month;
+
+use month::Month;
+
+pub struct Date {
+    year: u64,
+    month: Month,
+    day: u8
+}
+
+impl Date {
+    pub fn scottish_gaelic(&self) -> &str {
+        todo!()
     }
 }
---
+++ src/month.rs
@@ -0,0 +1,78 @@
+pub enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+use Month::*;
+
+impl Month {
+    /// Get the name of the month.
+    ///
+    /// This does not include any definite article ('the' in English), therefore
+    /// this should be used inside of a date for example. If the definite
+    /// article is needed, then you may call the
+    /// [`Month::scottish_gaelic_with_def_art()`] method.
+    ///
+    /// # Examples
+    /// ```
+    /// # use laithean::month::Month;
+    /// assert_eq!("Faoilleach", Month::January.scottish_gaelic());
+    /// ```
+    pub fn scottish_gaelic(&self) -> &str {
+        match self {
+            January => "Faoilleach",
+            February => "Gearran",
+            March => "Màrt",
+            April => "Giblean",
+            May => "Cèitean",
+            June => "Ògmhios",
+            July => "Iuchar",
+            August => "Lùnastal",
+            September => "Sultain",
+            October => "Dàmhair",
+            November => "Samhain",
+            December => "Dùbhlachd"
+        }
+    }
+
+    /// Get the name of the month with the definite article.
+    ///
+    /// The definite article is the "the" in English.
+    ///
+    /// # Parameters
+    /// - `cap` [bool] - Whether or not to capitalize the definite article.
+    ///
+    /// # Examples
+    /// ```
+    /// # use laithean::month::Month;
+    /// let uncapitalized = Month::January.scottish_gaelic_with_def_art(false);
+    /// assert_eq!("am Faoilleach", uncapitalized);
+    /// let capitalized = Month::January.scottish_gaelic_with_def_art(true);
+    /// assert_eq!("Am Faoilleach", capitalized);
+    /// ```
+    pub fn scottish_gaelic_with_def_art(&self, cap: bool) -> &str {
+        match self {
+            January => if cap { "Am Faoilleach" } else { "am Faoilleach" },
+            February => if cap { "An Gearran" } else { "an Gearran" },
+            March => if cap { "Am Màrt" } else { "am Màrt" },
+            April => if cap { "An Giblean" } else { "an Giblean" },
+            May => if cap { "An Cèitean" } else { "an Cèitean" },
+            June => if cap { "An t-Ògmhios" } else { "an t-Ògmhios" },
+            July => if cap { "An t-Iuchar" } else { "an t-Iuchar" },
+            August => if cap { "An Lùnastal" } else { "an Lùnastal" },
+            September => if cap { "An t-Sultain" } else { "an Sultain" },
+            October => if cap { "An Dàmhair" } else { "an Dàmhair" },
+            November => if cap { "An t-Samhain" } else { "an t-Samhain" },
+            December => if cap { "An Dùbhlachd" } else { "an Dùbhlachd" },
+        }
+    }
+}
